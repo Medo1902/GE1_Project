fdb1dc31dc4d01a6c16b27c6cff6434d587e0e25
Medo1902 <C20338661@mytudublin.ie>
2023-12-20 06:22:19 +0000

    removing unecessary stuff, adding some if statements

--- MagicianVR/addons/godot-xr-tools/objects/pickable.gd
+++ MagicianVR/addons/godot-xr-tools/objects/pickable.gd
@@ -221,7 +221,8 @@ func drop_and_free():
 # Called when this object is picked up
 func pick_up(by: Node3D) -> void:
 	
-	particle.emitting = true
+	if is_instance_valid(particle):
+		particle.emitting = true
 	# Skip if not enabled
 	if not enabled:
 		return
@@ -293,8 +294,8 @@ func pick_up(by: Node3D) -> void:
 
 # Called when this object is dropped
 func let_go(by: Node3D, p_linear_velocity: Vector3, p_angular_velocity: Vector3) -> void:
-	
-	particle.emitting = false
+	if is_instance_valid(particle):
+		particle.emitting = false
 	# Skip if not picked up
 	if not is_picked_up():
 		return

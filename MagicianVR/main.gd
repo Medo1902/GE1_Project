679891e8b1bad5963526edfd89e65be1d8b196cf
Medo1902 <C20338661@mytudublin.ie>
2023-12-20 12:40:17 +0000

    creating final scenes

--- MagicianVR/main.gd
+++ MagicianVR/main.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var xr_interface: XRInterface
+var current_scene_path = "res://scenes/main.tscn"
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -14,3 +15,16 @@ func _ready():
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+		
+	set_process_input(true)
+		
+func _input(event):
+	if event.is_action_pressed("reset_scene"):
+		# Reset the scene
+		reset_scene()
+
+func reset_scene():
+	var new_scene = load(current_scene_path).instance()
+	get_tree().root.remove_child(get_tree().current_scene)
+	get_tree().root.add_child(new_scene)
+	get_tree().current_scene = new_scene
